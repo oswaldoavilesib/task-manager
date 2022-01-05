1da9f38de346819b54a46d5d3d65f6b0b92ea3a3
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-05 14:03:00 -0600

    add front end validation of signup

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
 
+
+//----SIGN UP PAGE ROUTES----//
 /* GET signup page */
 router.get("/signup", (req, res, next) => {
 
     res.render("auth/signup");
 });
+
+
+
+
+
+
+
+//----log in PAGE ROUTES----//
+
+router.get("/signup", (req, res, next) => {
+
+    res.render("auth/signup");
+});
+
 
 module.exports = router;
