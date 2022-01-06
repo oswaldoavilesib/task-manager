5aaf06ef96406f586154f81bd7a2db711d6dd27a
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-06 09:55:12 -0600

    Sessions configuration done, including destroying session

---
+++ utils/route-guard.js
@@ -0,0 +1,18 @@
+const isLoggedOut = (req,res,next)=>{
+    if(!req.session.currentUser){
+        return res.redirect('/login')
+    }
+    next()
+}
+
+const isLoggedIn = (req,res,next)=>{
+    if(req.session.currentUser){
+        return res.redirect('/profile')
+    }
+    next()
+}
+
+module.exports = {
+    isLoggedIn,
+    isLoggedOut,
+}
