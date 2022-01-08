2d5bcc62a5d0d150575af5d33f26a53fe745200e
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-08 15:10:10 -0600

    Trying to use axios

--- routes/team/team.routes.js
+++ routes/team/team.routes.js
@@ -4,12 +4,12 @@ const clickUpService = require('../../service/index')
 
 const clickUpApiHandler = new clickUpService();
 
-router.get('/teams',isLoggedOut,(req,res)=>{
+router.get('/profile/teams',(req,res)=>{
     clickUpApiHandler
     .getTeams()
     .then(response => console.log(response.data))
     .catch(error => console.log('ERROR EN GETTING TEAMS FROM API',error))
-    res.send('HI')
+    res.render('private/teams')
 })
 
 
