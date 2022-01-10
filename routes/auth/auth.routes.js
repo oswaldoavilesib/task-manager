c4971060639052579ed83878feeba0452e2a0973
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-10 09:19:44 -0600

    Adding clickup code to current user

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -87,10 +87,7 @@ router.post('/login',async (req,res,next)=>{
 //----PROFILE PAGE ROUTES----//
 /* GET PROFILE page */
 router.get('/profile', isLoggedOut ,(req,res,next)=>{
-    //console.log(req.query.code)
-    req.session.currentUser.clickUpCode = req.query.code
-    console.log(req.session.currentUser.clickUpCode)
-    
+    //console.log(req.query.code
     res.render('private/profile',{user:req.session.currentUser})
 
     //console.log('clickUpCodeApi',clickUpCodeApi)
@@ -102,6 +99,8 @@ router.get('/profile', isLoggedOut ,(req,res,next)=>{
 router.get('/workspace',(req,res,next)=>{
     axios.post(`https://api.clickup.com/api/v2/oauth/token?code=${req.query.code}&client_id=MTQ6E6ABG2IQZHO4LSAGYKHKY2HAGWCC&client_secret=LRQU1S2ZFFLFAPVW1WYD5BI2DV2UFIBPRU6G4Z024IB01A33GI3598JA2828HWZL`)
     .then(response=>{
+        req.session.currentUser.clickUpCode = req.query.code
+        console.log(req.session.currentUser.clickUpCode)
         req.session.currentUser.clickUpAccessToken = response.data.access_token;
         console.log("ACCESSING TO ACCESS_TOKEN",req.session.currentUser.clickUpAccessToken)
         console.log('req.ses WITH TOKENS',req.session)
