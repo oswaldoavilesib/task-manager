f1ff0af9de8eb3e57d492d2cd22086dd7c7e279b
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-10 10:16:03 -0600

    CAlling teams api endpoing not working. Console.log currentuser to see if it gets with the access token

--- routes/team/team.routes.js
+++ routes/team/team.routes.js
@@ -6,15 +6,15 @@ const clickUpService = require('../../service/index')
 const clickUpApiHandler = new clickUpService();
 
 router.get('/profile/teams',(req,res)=>{
-    const accessToken = req.session.currentUser.clickUpAccessToken
-    axios.get(`https://api.clickup.com/api/v2/team`,{
-        headers: {
-            'Authorization': accessToken,
-        }
-    })
-    .then(response=>{
-        res.render(response)
-    })
+    console.log(req.session.currentUser.clickUpAccessToken)
+    // axios.get(`https://api.clickup.com/api/v2/team`,{
+    //     headers: {
+    //         'Authorization': accessToken,
+    //     }
+    // })
+    // .then(response=>{
+    //     res.render(response)
+    // })
     //res.render('private/teams')
 })
 
