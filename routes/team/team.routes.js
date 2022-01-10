06db8dddc878215b742ea0a29a5ca46c58096b38
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-10 12:45:26 -0600

    added 'team_id=' to endpoint request to see if it works

--- routes/team/team.routes.js
+++ routes/team/team.routes.js
@@ -23,7 +23,7 @@ router.get('/profile/teams',(req,res)=>{
 //----GET ALL SPACES WORKSPACE"-----//
 router.get('/profile/spaces',(req,res,next)=>{
     const accessToken = req.session.currentUser.clickUpAccessToken;
-    axios.get('https://api.clickup.com/api/v2/team/12602813/space?archived=false',{
+    axios.get('https://api.clickup.com/api/v2/team/team_id=12602813/space?archived=false',{
         'Authorization': accessToken
     })
     .then(response=>console.log(response.data))
