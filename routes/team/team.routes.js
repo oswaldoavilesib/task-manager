1694431354f6ae915e6891c725441a6aeb3a6c91
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-10 10:28:52 -0600

    Trying to get console.log of the response of teams endpoint

--- routes/team/team.routes.js
+++ routes/team/team.routes.js
@@ -7,14 +7,16 @@ const clickUpApiHandler = new clickUpService();
 
 router.get('/profile/teams',(req,res)=>{
     console.log(req.session.currentUser.clickUpAccessToken)
-    // axios.get(`https://api.clickup.com/api/v2/team`,{
-    //     headers: {
-    //         'Authorization': accessToken,
-    //     }
-    // })
-    // .then(response=>{
-    //     res.render(response)
-    // })
+    const accessToken = req.session.currentUser.clickUpAccessToken
+    console.log('accessToken: ',accessToken)
+    axios.get(`https://api.clickup.com/api/v2/team`,{
+        headers: {
+            'Authorization': accessToken,
+        }
+    })
+    .then(response=>{
+        res.render(response)
+    })
     res.render('private/teams')
 })
 
