c85a5a9472de6433f177284b97753575d9286e36
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-10 11:42:56 -0600

    TEAMS APIHANDLER WORKING

--- routes/team/team.routes.js
+++ routes/team/team.routes.js
@@ -14,20 +14,6 @@ router.get('/profile/teams',(req,res)=>{
         res.render('private/teams')
     })
     .catch(error => console.log("ERROR GETTING TEAMS FROM ENDPOINT",error) )
-
-    // try {
-    //     console.log(req.session.currentUser.clickUpAccessToken)
-    //     const accessToken = req.session.currentUser.clickUpAccessToken
-    //     const response = await axios.get('https://api.clickup.com/api/v2/team',{
-    //         headers:{
-    //             'Authorization': accessToken,
-    //         }
-    //     })
-    //     console.log(response.data)
-    //     res.render('private/teams')
-    // } catch(error){
-    //     console.log('ERROR EN TEAMS ENDPOINT FROM API',error)
-    // }
 })
 
 
