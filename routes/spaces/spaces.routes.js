bc2b3459252762c720216c971715de85a351449b
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-10 13:42:16 -0600

    testing new basic front of spaces

--- routes/spaces/spaces.routes.js
+++ routes/spaces/spaces.routes.js
@@ -17,14 +17,6 @@ router.get('/profile/spaces',(req,res,next)=>{
         res.render('private/spaces',{spaces:response.data.spaces})
     })
     .catch(error=>console.log("ERROR EN GET SPACES API",error))
-
-    // clickUpApiHandler
-    // .getSpaces(accessToken)
-    // .then(response=>{
-    //     console.log(response.data)
-    //     res.render('private/spaces')
-    // })
-    // .catch(error=>console.log("ERROR EN ENDPOING TO GET SPACES",error))
 })
 
 module.exports = router;
