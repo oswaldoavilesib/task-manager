3f55a56334a14d5bc04d040bd800c2f80a62d9b2
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-10 09:07:43 -0600

    Trying to fix bug creating another route and redirecting there after clickup validation

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -92,25 +92,22 @@ router.get('/profile', isLoggedOut ,(req,res,next)=>{
     console.log(req.session.currentUser.clickUpCode)
     
     res.render('private/profile',{user:req.session.currentUser})
+    //console.log('clickUpCodeApi',clickUpCodeApi)
+})
 
+
+//----WORKSPACE PAGE ROUTES----//
+/* GET WORKSPACE page */
+router.get('/workspace',(req,res,next)=>{
     axios.post(`https://api.clickup.com/api/v2/oauth/token?code=${req.query.code}&client_id=MTQ6E6ABG2IQZHO4LSAGYKHKY2HAGWCC&client_secret=LRQU1S2ZFFLFAPVW1WYD5BI2DV2UFIBPRU6G4Z024IB01A33GI3598JA2828HWZL`)
     .then(response=>{
         req.session.currentUser.clickUpAccessToken = response.data.access_token;
         console.log("ACCESSING TO ACCESS_TOKEN",req.session.currentUser.clickUpAccessToken)
         console.log('req.ses WITH TOKENS',req.session)
+        res.render('private/workspace')
     })
     .catch(error=>console.log('ERROR EN GET TOKE ACCESS FROM CLICKUP API',error))
-    //console.log('clickUpCodeApi',clickUpCodeApi)
-})
 
-  /* POST PROFILE page */
-// router.post('/profile', isLoggedOut ,(req,res,next)=>{
-// })
-
-//----WORKSPACE PAGE ROUTES----//
-/* GET WORKSPACE page */
-router.get('/workspace',(req,res,next)=>{
-    res.render('private/workspace')
 })
 
 
