f6b70a5f4bffc1ae8d9d751c8c2b416b20ebdf4d
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-10 09:35:43 -0600

    Accesing Teams faild. Looking a t what the res.body comes frome and move render of private/workspace inside the promise

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -100,13 +100,12 @@ router.get('/workspace',(req,res,next)=>{
     axios.post(`https://api.clickup.com/api/v2/oauth/token?code=${req.query.code}&client_id=MTQ6E6ABG2IQZHO4LSAGYKHKY2HAGWCC&client_secret=LRQU1S2ZFFLFAPVW1WYD5BI2DV2UFIBPRU6G4Z024IB01A33GI3598JA2828HWZL`)
     .then(response=>{
         req.session.currentUser.clickUpCode = req.query.code
-        console.log(req.session.currentUser.clickUpCode)
         req.session.currentUser.clickUpAccessToken = response.data.access_token;
-        console.log("ACCESSING TO ACCESS_TOKEN",req.session.currentUser.clickUpAccessToken)
         console.log('req.ses WITH TOKENS',req.session)
+        console.log(res.body)
+        res.render('private/workspace')
     })
     .catch(error=>console.log('ERROR EN GET TOKE ACCESS FROM CLICKUP API',error))
-    res.render('private/workspace')
 })
 
 
