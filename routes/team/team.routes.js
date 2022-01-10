9c503c305c9d7b750f8eaa71a1331ce64014b693
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-10 13:38:36 -0600

    Delete repeated code from team routes

--- routes/team/team.routes.js
+++ routes/team/team.routes.js
@@ -19,26 +19,4 @@ router.get('/profile/teams',(req,res)=>{
     .catch(error => console.log("ERROR GETTING TEAMS FROM ENDPOINT",error) )
 })
 
-
-//----GET ALL SPACES WORKSPACE"-----//
-router.get('/profile/spaces',(req,res,next)=>{
-    const accessToken = req.session.currentUser.clickUpAccessToken;
-
-    clickUpApiHandler
-    .getSpaces(accessToken)
-    .then(response=>{
-        console.log(response.data)
-        res.render('private/spaces',{spaces:response.data.spaces})
-    })
-    .catch(error=>console.log("ERROR EN GET SPACES API",error))
-
-    // clickUpApiHandler
-    // .getSpaces(accessToken)
-    // .then(response=>{
-    //     console.log(response.data)
-    //     res.render('private/spaces')
-    // })
-    // .catch(error=>console.log("ERROR EN ENDPOING TO GET SPACES",error))
-})
-
 module.exports = router;
