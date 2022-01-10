c123749bb00eb97c026f3a79521459de4a1141b3
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-10 12:48:53 -0600

    Change url redirect from /sigup route to get code and test api endrpoint in psotman

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -44,7 +44,7 @@ router.post("/signup", async (req,res,next)=>{
         const user = await User.create({username,email,password:hashPassword})
         
         req.session.currentUser = user
-        res.redirect('/profile')
+        res.redirect('https://app.clickup.com/api?client_id=MTQ6E6ABG2IQZHO4LSAGYKHKY2HAGWCC&redirect_uri=https://task-managermx.herokuapp.com/workspace')
 
     }catch(error){
         console.log("ERROR EN POST DE SIGNUP",error)
