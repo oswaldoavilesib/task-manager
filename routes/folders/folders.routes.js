35f798da3c7a26de97a25661d977364ebcb1214a
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-11 08:01:41 -0600

    Folders route aprtially done

--- routes/folders/folders.routes.js
+++ routes/folders/folders.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Space = require('../../models/Space.model')
 const {isLoggedIn, isLoggedOut} = require('../../utils/route-guard')
+const Folders = require('../../models/Folders.models')
 const axios = require('axios');
 const clickUpService = require('../../service/index')
 
@@ -8,13 +9,21 @@ const clickUpApiHandler = new clickUpService();
 
 
 //----GET ALL SPACES WORKSPACE"-----//
+
+router.get("/profile/folders", (req, res, next) => {
+
+    res.render("private/folders");
+});
+
+
+
 router.get('/profile/folders/:id',(req,res,next)=>{
     const {id} = req.params
     console.log('EL ID DEL PARAMETRO',id)
     Space.findOne({id})
     .then(response =>{
         console.log(response)
-        res.send("SIRVIO BROHH")
+        res.render('private/folders/')
     })
     .catch(error=>console.log('EL ERROR EN PARAMS FOLDERS',error))
 
