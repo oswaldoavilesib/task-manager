faeb04682e32c0da27f13ead892bf4b732c563a1
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-11 15:33:24 -0600

    Trying a new way of getting folders from a space

--- routes/folders/folders.routes.js
+++ routes/folders/folders.routes.js
@@ -10,10 +10,30 @@ const clickUpApiHandler = new clickUpService();
 
 //----GET ALL FOLDERS FROM A WORKSPACE"-----//
 
-router.get("/profile/folders", (req, res, next) => {
+// router.get("/profile/folders", (req, res, next) => {
+//     Space.findOne({id})
+//     .then(response =>{
+//         console.log(response)
+//         console.log(response.id)
+//         clickUpApiHandler
+//         .getFolders(response.id,accessToken)
+//         .then(response => {
+//             console.log(response)
+//             res.redirect('/private/folders')
+//         })
+//         .catch(error=>console.log("ERROR EN GET FOLDER PROMISE",error))
 
-    res.render("private/folders");
-});
+
+//     res.render("private/folders");
+// });
+
+
+
+
+
+
+
+//----TRYING TO GET THE ID OF AN SPACE FOM A DATABASE TO SENDIT TO THE GETFOLDERS APIHANDLER------//
 
 router.get('/profile/folders/:id',(req,res,next)=>{
     const {id} = req.params
@@ -27,6 +47,7 @@ router.get('/profile/folders/:id',(req,res,next)=>{
         .getFolders(response.id,accessToken)
         .then(response => {
             console.log(response)
+           res.render('/private/folders')
         })
         .catch(error=>console.log("ERROR EN GET FOLDER PROMISE",error))
 
