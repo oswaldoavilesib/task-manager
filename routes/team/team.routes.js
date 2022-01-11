0c5a49f02ab1f2664409064e8752a6122c8bbdc3
Oswaldo <oswaldoavilesib@gmail.com>
2022-01-10 20:29:21 -0600

    add saveAccesstoken as a callback on teams

--- routes/team/team.routes.js
+++ routes/team/team.routes.js
@@ -10,10 +10,9 @@ const clickUpApiHandler = new clickUpService();
 router.get('/profile/teams',(req,res)=>{
     const accessToken = req.session.currentUser.clickUpAccessToken;
     clickUpApiHandler
-    .getTeams()
+    .getTeams(this.saveAccessToken)
     .then(response => {
         console.log(response.data)
-        clickUpApiHandler.saveAccessToken(response.data.access_token)
         res.render('private/teams',{teams: response.data.teams})
     })
     .catch(error => console.log("ERROR GETTING TEAMS FROM ENDPOINT",error) )
