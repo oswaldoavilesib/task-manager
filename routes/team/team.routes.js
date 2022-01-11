028d4c4d430e1359ae54d7028c88737ca8aed6e4
Oswaldo <oswaldoavilesib@gmail.com>
2022-01-10 20:45:38 -0600

    testing apihandler getTEams

--- routes/team/team.routes.js
+++ routes/team/team.routes.js
@@ -10,7 +10,7 @@ const clickUpApiHandler = new clickUpService();
 router.get('/profile/teams',(req,res)=>{
     const accessToken = req.session.currentUser.clickUpAccessToken;
     clickUpApiHandler
-    .getTeams(this.saveAccessToken)
+    .getTeams()
     .then(response => {
         console.log(response.data)
         res.render('private/teams',{teams: response.data.teams})
