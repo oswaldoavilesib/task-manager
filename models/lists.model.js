51b96075e8b773a0ff62ea84ba565e6cdaa18d3d
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-01-12 08:13:42 -0600

    list model done

---
+++ models/lists.model.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+const listSchema = new Schema({
+    id: {
+        type: Number
+    },
+    name:{
+        type: String,
+    },
+    _user: {
+        type:Schema.Types.ObjectId, ref:"User"
+    },
+    _space: {
+        type:Schema.Types.ObjectId, ref:"Space"
+    },
+
+    _folder: {
+        type:Schema.Types.ObjectId, ref:"Space"
+    },
+
+    _team: {
+        type:Schema.Types.ObjectId, ref:"Team"
+    }
+},
+{
+    timestamps: true,
+
+})
+
+module.exports = model('List',listSchema)
