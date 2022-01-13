ba708dc0dd0fc63d0ac1d0462adfdbcdec62bbc1
Oswaldo <oswaldoavilesib@gmail.com>
2022-01-12 22:03:04 -0600

    Front of tasks done again

--- routes/tasks/tasks.js
+++ routes/tasks/tasks.js
@@ -9,14 +9,33 @@ const clickUpApiHandler = new clickUpService();
 
 //----GET ALL TASKS FROM A LIST"-----//
 router.get('/profile/tasks/:id',(req,res,next)=>{
-    const accessToken = req.session.currentUser.clickUpAccessToken;
-    const {id} = req.params
+    const accessToken = req.session.currentUser.clickUpAccessToken; 
+    const {id} = req.params // We get the ID of the LIST from our DB
 
+    //CLICKUP API HANDLER STARTS HERE
     clickUpApiHandler
-    .getTasks(id,accessToken)
+    .getTasks(id,accessToken) //WE pass the ID of the list from the database
     .then(response=>{
-        console.log(response.data)
-        res.render('private/tasks')
+        console.log("RESPONSE:DATA FROM TASKS",response.data) //We recieve al th data from api call
+
+        //Now we neet to iterate in each of the tasks to make sure they are on our database and if they are, do not add the, again
+        response.data.tasks.forEAch((task => {
+            const {id,name,...rest} = task
+            Task.find({id: {$eq:id}})
+            .then(response => {
+                console.log("RESPONSE FROM LIST.FINDONE",response)
+                if(!response.length){
+                    List.create({id,name})
+                    .then(response => console.log('We created a newTask',response))
+                    .catch(error => console.log("ERROR EN ADDING A TASK ON DB",error))
+                } else {
+                    console.log("This Task is already on db")
+                }
+            })
+            .catch(error => console.log("ERROR EN FINDING TASKS IN DB",error))
+        }))
+
+        res.render('private/tasks',{tasks: response.data.tasks})
     })
     .catch(error => console.log("ERROR EN GET TASKS API",error))
 })
