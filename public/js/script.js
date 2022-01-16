e7c124a29c014ac8cbd889ea1cf84fa6d2ce072c
Oswaldo <oswaldoavilesib@gmail.com>
2022-01-15 21:48:45 -0600

    Add due dates tot asks

--- public/js/script.js
+++ public/js/script.js
@@ -6,9 +6,19 @@ document.addEventListener(
   false
 );
 
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+// var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
+
+<script type="text/javascript">
+   $(document).on("click", ".open-modal", function () {
+	 var x = new Date(); 
+     var myHeading = "<p>I Am Added Dynamically </p>";
+     $("#modal-body").html(myHeading + x);     
+     $('#modal').modal('show');
+    });
+
+</script>
