66f199edbefd8fdf031f924614728476049100ef
Oswaldo <oswaldoavilesib@gmail.com>
2022-01-18 18:12:24 -0600

    testing update route

---
+++ routes/update/update.routes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const Task = require('../../models/Task.model')
+const {isLoggedIn, isLoggedOut} = require('../../utils/route-guard')
+const axios = require('axios');
+const clickUpService = require('../../service/index')
+
+const clickUpApiHandler = new clickUpService();
+
+
+//-------   UPDATE ROUTES------///
+
+//GET update uri //
+router.get('/profile/tasks/update/:id',(req,res,next)=>{
+    const accessToken = req.session.currentUser.clickUpAccessToken; 
+    const {id} = req.params;
+    Task.find({id: {$eq:id}})
+    .then(response => {
+        console.log("RESPONSE OF FIND TASK TO EDIT",response)
+        res.render('private/update',{tasks:response,id})
+    })
+    .catch(error=>console.log("ERROR EN FIND TASK TO EDIT",error))
+})
+
+router.post('/profile/tasks/update/:id',(req,res,next)=>{
+    const accessToken = req.session.currentUser.clickUpAccessToken; 
+    console.log("REQ.BODY DE POST TASK UPDATE",req.body)
+    
+    console.log("REQ.BODY DE POST TASK UPDATE",req.body.dueDate.value)
+    res.redirect("back")
+})
+
+
+
+
+
+module.exports = router;
