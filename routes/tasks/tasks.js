dd15931c550d90c6672db8c8e6066224bdec0cad
Oswaldo <oswaldoavilesib@gmail.com>
2022-01-21 10:28:13 -0600

    testing

--- routes/tasks/tasks.js
+++ routes/tasks/tasks.js
@@ -56,7 +56,6 @@ router.get('/profile/tasks/:id',(req,res,next)=>{
             })
             .catch(error => console.log("ERROR EN FINDING TASKS IN DB",error))
         }))
-        console.log("DATEFINAL",dateFinal)
         res.render('private/tasks',{tasks: response.data.tasks,id})
     })
     .catch(error => console.log("ERROR EN GET TASKS API",error))
