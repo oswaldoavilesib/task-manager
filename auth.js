21becf851f38226364178d70a242845b09cd0343
Oswaldo <oswaldoavilesib@gmail.com>
2022-01-22 12:21:17 -0600

    change google redirection

--- auth.js
+++ auth.js
@@ -10,7 +10,7 @@ const User = require("./models/User.model")
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback",
+    callbackURL: "https://task-managermx.herokuapp.com/profile",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
